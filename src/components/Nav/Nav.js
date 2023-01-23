9132a9bd12850706393b30ddf0b9ca0f77c189ca
leoarthurwall <leowall93@gmail.com>
2023-01-23 16:52:36 +0000

    created a first draft for the nav bar

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,10 +1,23 @@
-import React from 'react'
-import styles from "./Nav.module.css"
+import React from "react";
+import styles from "./Nav.module.css";
 
 const Nav = () => {
   return (
-    <nav className={styles.container}>Nav</nav>
-  )
-}
+    <nav className={styles.container}>
+      <h2 className={styles.logo}>J/M</h2>
+      <ul className={styles.list}>
+        <li className={styles.item}>Home</li>
+        <li className={styles.item}>About</li>
+        <li className={styles.item}>Services</li>
+        <li className={styles.item}>Contact</li>
+      </ul>
+      <ul className={styles.contactList}>
+        <li className={styles.contact}>Email: JAKEMAYEXTERIORS@GMAIL.COM</li>
+        <li className={styles.contact}>PHONE: 07946430774 </li>
 
-export default Nav
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;

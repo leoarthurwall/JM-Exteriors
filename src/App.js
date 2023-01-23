1b6520da9d2e9b607b0e61b8cb270c8b5349ccde
leoarthurwall <leowall93@gmail.com>
2023-01-23 16:39:06 +0000

    set the nav to be fixed with z index of 10

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Home from "./components/Home/Home";
+import Nav from "./components/Nav/Nav";
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <Home />
     </div>
   );

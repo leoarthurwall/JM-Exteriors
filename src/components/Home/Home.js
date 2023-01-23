1802dfb6795f568232c44ac7b9fe557d278fe362
leoarthurwall <leowall93@gmail.com>
2023-01-23 16:16:10 +0000

    added header text for the home section

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,15 @@
-import React from 'react'
-import styles from "./Home.module.css"
+import React from "react";
+import styles from "./Home.module.css";
 
 const Home = () => {
   return (
-    <div className={styles.container}></div>
-  )
-}
+    <section className={styles.container}>
+      <div className={styles.headerWrapper}>
+        <h1 className={styles.title}> JM Exteriors</h1>
+        <h3 className={styles.subHeading}>Glazier & Exertior Professional</h3>
+      </div>
+    </section>
+  );
+};
 
-export default Home
+export default Home;

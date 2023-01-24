aace1500813632c1d795a31535436d9b53e5ae94
leoarthurwall <leowall93@gmail.com>
2023-01-24 20:48:27 +0000

    added images and mapped through them

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -2,58 +2,49 @@ import React from "react";
 import styles from "./Services.module.css";
 
 const Services = () => {
-   const ServiceImage = [
-        {
-            header: "Windows",
-            bgImage: "../../public/images/window.jpeg",
-        },
-        {
-            header: "Doors",
-            bgImage: "",
-        },
-        {
-            header: "Conservatories",
-            bgImage: "",
-        },
-        {
-            header: "Guttering",
-            bgImage: "",
-        },
-        {
-            header: "Fasias & Soffits",
-            bgImage: "",
-        },
-        {
-            header: "Cladding",
-            bgImage: "",
-        }
-    ]
+  const serviceImage = [
+    {
+      header: "Windows",
+      bgImage: "images/window.jpeg",
+    },
+    {
+      header: "Doors",
+      bgImage: "images/door.jpeg",
+    },
+    {
+      header: "Conservatories",
+      bgImage: "images/conservatory-2.jpeg",
+    },
+    {
+      header: "Guttering",
+      bgImage: "images/guttering.jpeg",
+    },
+    {
+      header: "Fasias & Soffits",
+      bgImage: "images/fasia&soffit.jpeg",
+    },
+    {
+      header: "Cladding",
+      bgImage: "images/cladding.jpeg",
+    },
+  ];
 
   return (
     <section className={styles.container}>
       <div className={styles.gridContainer}>
-        {ServiceImage.map((service, i) => {
-        <img src={service.bgImage} alt="hello" className={styles.gridItem}>
-            <h2 className={styles.itemHeader}>{service.header}</h2>
-        
-        </img >
-
+        {serviceImage.map((service, index) => {
+          return (
+            <div className={styles.gridItem}>
+              <img
+                key={index}
+                src={service.bgImage}
+                alt={service.header}
+                className={styles.image}
+              ></img>
+              <h2 className={styles.itemHeader}>{service.header}</h2>
+            </div>
+          );
         })}
-        <div className={styles.gridItem}>IMG</div>
-        <div className={styles.gridItem}>IMG</div>
-        <ul className={styles.gridItem}>
-          <li>Windows</li>
-          <li>Doors</li>
-          <li>Conservatories</li>
-          <li>Guttering</li>
-          <li>Facias & Soffits</li>
-          <li>Classing</li>
-        </ul>
-
-        <div className={styles.gridItem}>
-          Offers services across the island. Call for a free quote now.
-        </div>
-        <div className={styles.gridItem}>IMG</div>
       </div>
     </section>
   );

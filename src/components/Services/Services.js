c4785c66e75f823efded8450ce965bf97ccef0d7
leoarthurwall <leowall93@gmail.com>
2023-01-24 17:59:10 +0000

    created a service image array, each object includes header and bgImage. this will be mapped over

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -2,10 +2,41 @@ import React from "react";
 import styles from "./Services.module.css";
 
 const Services = () => {
+   const ServiceImage = [
+        {
+            header: "Windows",
+            bgImage: "",
+        },
+        {
+            header: "Doors",
+            bgImage: "",
+        },
+        {
+            header: "Conservatories",
+            bgImage: "",
+        },
+        {
+            header: "Guttering",
+            bgImage: "",
+        },
+        {
+            header: "Fasias & Soffits",
+            bgImage: "",
+        },
+        {
+            header: "Cladding",
+            bgImage: "",
+        }
+    ]
+
   return (
     <section className={styles.container}>
+
       <div className={styles.gridContainer}>
-        <div className={styles.gridItem}>Services</div>
+        <div className={styles.gridItem}>
+            <h2 className={styles.itemHeader}></h2>
+        
+        </div>
         <div className={styles.gridItem}>IMG</div>
         <div className={styles.gridItem}>IMG</div>
         <ul className={styles.gridItem}>
@@ -17,10 +48,10 @@ const Services = () => {
           <li>Classing</li>
         </ul>
 
-        <div className={styles.gridItem}>IMG</div>
         <div className={styles.gridItem}>
           Offers services across the island. Call for a free quote now.
         </div>
+        <div className={styles.gridItem}>IMG</div>
       </div>
     </section>
   );

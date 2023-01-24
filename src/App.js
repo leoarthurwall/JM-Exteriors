ab914735f9ddfd4ee17eb3a49d03ad0ca3ec82b9
leoarthurwall <leowall93@gmail.com>
2023-01-24 21:28:53 +0000

    created an overlap using absolute height for the services section

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import About from "./components/About/About";
+import Contact from "./components/Contact/Contact";
 import Home from "./components/Home/Home";
 import Nav from "./components/Nav/Nav";
 import Services from "./components/Services/Services";
@@ -11,6 +12,7 @@ function App() {
       <Home />
       <About />
       <Services />
+      <Contact />
     </div>
   );
 }
---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,22 @@
+import React from "react";
+import styles from "./Contact.module.css";
+
+const Contact = () => {
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.header}>Welcome to JM Exteriors</h2>
+      <p className={styles.para}>
+        JM Exteriors Ltd specialise in the installation of quality PVCu,
+        aluminium and timber double glazed windows, doors & conservatories. We
+        also supply and fit fascias and soffits. We have many satiffied
+        customers across the Isle of Wight. <br /> With over 10 years of
+        professional experience we started to supply quality windows and doors.
+        A small family business offering a specialist glazing service in the
+        Isle of Wight. Service and quality have built the business through
+        reputation for competitive prices and reliability.
+      </p>
+    </div>
+  );
+};
+
+export default Contact;

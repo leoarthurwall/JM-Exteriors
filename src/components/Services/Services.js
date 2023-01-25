a0c117a8a37fc35dce1abbe483f9bef5df68f2ef
leoarthurwall <leowall93@gmail.com>
2023-01-25 21:14:17 +0000

    styled the sections component

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -6,26 +6,32 @@ const Services = () => {
     {
       header: "Windows",
       bgImage: "images/window.jpeg",
+      text: "We offer a wide range of window styles and materials, including uPVC, aluminum, and timber. Our windows are energy-efficient and customizable to fit your specific needs and preferences.",
     },
     {
       header: "Doors",
       bgImage: "images/door.jpeg",
+      text: "We offer a variety of door styles, including French, sliding, and bi-folding. We work with the best manufacturers to provide durable, secure, and energy-efficient doors.",
     },
     {
       header: "Conservatories",
       bgImage: "images/conservatory-2.jpeg",
+      text: "Our conservatories are designed to provide an elegant and functional extension of your home. We offer a range of styles and materials to choose from, and our team will work with you to create a space that meets your specific needs and budget.",
     },
     {
       header: "Guttering",
       bgImage: "images/guttering.jpeg",
+      text: "We offer professional guttering services including installation, repair and maintenance. We use high-quality materials to ensure durability and effectiveness in preventing water damage.",
     },
     {
       header: "Fasias & Soffits",
       bgImage: "images/fasia&soffit.jpeg",
+      text: "We provide professional guttering, soffits and facias installation to keep your property protected from water damage. We use high-quality materials and ensure that your installation is done to the highest standard.",
     },
     {
       header: "Cladding",
       bgImage: "images/cladding.jpeg",
+      text: "Our cladding services are a great way to improve the appearance and energy efficiency of your home. We offer a variety of materials, including PVC, wood, and aluminum.",
     },
   ];
 
@@ -41,7 +47,10 @@ const Services = () => {
                 alt={service.header}
                 className={styles.image}
               ></img>
-              <h2 className={styles.itemHeader}>{service.header}</h2>
+              <div className={styles.imageTextWrapper}>
+                <h2 className={styles.itemHeader}>{service.header}</h2>
+                <p className={styles.itemText}>{service.text}</p>
+              </div>
             </div>
           );
         })}

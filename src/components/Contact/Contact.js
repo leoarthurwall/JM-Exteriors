1d1e05eaa2d2ab6c85188b77a100117b5fa92e5d
leoarthurwall <leowall93@gmail.com>
2023-01-25 23:34:46 +0000

    created a footer component

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -11,21 +11,7 @@ const Contact = () => {
         feel free to contact us. We look forward to hearing from you and helping
         you enhance the beauty and energy efficiency of your home.
       </p>
-      <ul className={styles.ul}>
-        <li className={styles.list}>
-          <span className={styles.span}>Phone: </span>07946430774
-        </li>
-        <li className={styles.list}>
-          <span className={styles.span}>Email:</span> jakemayexteriors@gmail.com
-        </li>
-        <li className={styles.list}>
-          <span className={styles.span}>Address: </span>Isle of Wight
-        </li>
-        <li className={styles.list}>
-          <span className={styles.span}>Business Hours: </span>08:00-17:00,
-          Mon-Fri
-        </li>
-      </ul>
+      
       <p className={styles.para}>
         Thank you for choosing JM Exteriors for your window fitting needs. We
         are committed to providing you with the best service and quality
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,30 @@
+import React from "react";
+import styles from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <div className={styles.container}>
+      <h3 className={styles.logo}>JM Exteriors</h3>
+      <div className={styles.right}>
+        <ul className={styles.ul}>
+          <li className={styles.list}>
+            <span className={styles.span}>Phone: </span>07946430774
+          </li>
+          <li className={styles.list}>
+            <span className={styles.span}>Email:</span>{" "}
+            jakemayexteriors@gmail.com
+          </li>
+          <li className={styles.list}>
+            <span className={styles.span}>Address: </span>Isle of Wight
+          </li>
+          <li className={styles.list}>
+            <span className={styles.span}>Business Hours: </span>08:00-17:00,
+            Mon-Fri
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;

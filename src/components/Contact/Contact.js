a63a8e39161031c3eed9b57424366e7fb54131f9
leoarthurwall <leowall93@gmail.com>
2023-01-25 20:27:45 +0000

    updated content for contact

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -4,17 +4,32 @@ import styles from "./Contact.module.css";
 const Contact = () => {
   return (
     <div className={styles.container}>
-      <h2 className={styles.header}>Contact</h2>
+      <h2 className={styles.header}>Contact us</h2>
       <p className={styles.para}>
-        It's my job to provide you with the best possible service. If you would
-        like a free quote, simply get in touch by phone or email:
+        Thank you for considering JM Exteriors for your window fitting needs. If
+        you have any questions or would like to schedule a consultation, please
+        feel free to contact us. We look forward to hearing from you and helping
+        you enhance the beauty and energy efficiency of your home.
       </p>
+      <ul className={styles.ul}>
+        <li className={styles.list}>
+          <span className={styles.span}>Phone: </span>07946430774
+        </li>
+        <li className={styles.list}>
+          <span className={styles.span}>Email:</span> jakemayexteriors@gmail.com
+        </li>
+        <li className={styles.list}>
+          <span className={styles.span}>Address: </span>Isle of Wight
+        </li>
+        <li className={styles.list}>
+          <span className={styles.span}>Business Hours: </span>08:00-17:00,
+          Mon-Fri
+        </li>
+      </ul>
       <p className={styles.para}>
-        <span className={styles.span}>Email: </span>
-        jakemayexteriors@gmail.com
-      </p>
-      <p className={styles.para}>
-        <span className={styles.span}>Phone:</span> 07946430774{" "}
+        Thank you for choosing JM Exteriors for your window fitting needs. We
+        are committed to providing you with the best service and quality
+        workmanship.
       </p>
     </div>
   );

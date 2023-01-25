0f25de40e3d93dd038524cab81624226a15c7905
leoarthurwall <leowall93@gmail.com>
2023-01-25 23:40:29 +0000

    added basic styling for the footer layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import About from "./components/About/About";
 import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
 import Home from "./components/Home/Home";
 import Nav from "./components/Nav/Nav";
 import Services from "./components/Services/Services";
@@ -13,6 +14,7 @@ function App() {
       <About />
       <Services />
       <Contact />
+      <Footer />
     </div>
   );
 }

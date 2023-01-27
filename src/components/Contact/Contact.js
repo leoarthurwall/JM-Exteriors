b283f1fcaccd196b9ed23064733b0aba7f45a2e7
leoarthurwall <leowall93@gmail.com>
2023-01-27 17:42:46 +0000

    contact - call to actions, removed button and styled atag so all button is now clickable

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -12,17 +12,20 @@ const Contact = () => {
         you enhance the beauty and energy efficiency of your home.
       </p>
 
+      <div className={styles.buttonWrapper}>
+        <a href="tel:07946430774" className={styles.aText}>
+          Make a call
+        </a>
+
+        <a href="mailto:jakemayexteriors@gmail.com" className={styles.aText}>
+          Send an Email
+        </a>
+      </div>
       <p className={styles.para}>
         Thank you for choosing JM Exteriors for your window fitting needs. We
         are committed to providing you with the best service and quality
         workmanship.
       </p>
-      <button className={styles.button}>
-        <a href="tel:07946430774">Call for a free quote</a>
-      </button>
-      <button className={styles.button}>
-        <a href="mailto:jakemayexteriors@gmail.com">Send Email</a>
-      </button>
     </div>
   );
 };

60025f1a9572b7c1fde66e1761ab55a8819fcbe7
leoarthurwall <leowall93@gmail.com>
2023-01-27 16:04:20 +0000

    added content and styled it on the menu

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -3,7 +3,14 @@ import styles from "./Menu.module.css";
 
 const Menu = ({ isOpen }) => {
   return (
-    <div className={isOpen ? styles.container : styles.container2}>Menu</div>
+    <div className={isOpen ? styles.containerOpen : styles.containerClosed}>
+        <ul className={styles.list}>
+          <li className={styles.item}>Home</li>
+          <li className={styles.item}>About</li>
+          <li className={styles.item}>Services</li>
+          <li className={styles.item}>Contact</li>
+        </ul>
+    </div>
   );
 };
 

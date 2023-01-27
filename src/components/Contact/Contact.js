2d88166a2092e019218094415c05f4f38b193060
leoarthurwall <leowall93@gmail.com>
2023-01-27 17:20:56 +0000

    Contact - added buttons for calling and emailing. need to style

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -11,13 +11,18 @@ const Contact = () => {
         feel free to contact us. We look forward to hearing from you and helping
         you enhance the beauty and energy efficiency of your home.
       </p>
-      
+
       <p className={styles.para}>
         Thank you for choosing JM Exteriors for your window fitting needs. We
         are committed to providing you with the best service and quality
         workmanship.
       </p>
-      <a href="tel:07946430774">Call for a free consultation</a>
+      <button className={styles.button}>
+        <a href="tel:07946430774">Call for a free quote</a>
+      </button>
+      <button className={styles.button}>
+        <a href="mailto:jakemayexteriors@gmail.com">Send Email</a>
+      </button>
     </div>
   );
 };

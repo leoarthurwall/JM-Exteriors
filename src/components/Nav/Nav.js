77cd590590cee52d0eccb829183cb72a9ed10900
leoarthurwall <leowall93@gmail.com>
2023-02-05 11:07:23 +0100

    nav - removed unused css. fixed menu icon change error

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,13 +1,10 @@
 import React from "react";
 import styles from "./Nav.module.css";
-import { useMediaQuery } from "react-responsive";
 import { HiOutlineMenu } from "react-icons/hi";
 import { AiOutlineClose } from "react-icons/ai";
 
 const Nav = ({ handleMenuClick, isOpen }) => {
-  const isMobile = useMediaQuery({
-    query: "(max-width: 550px)",
-  });
+
 
   return (
     <nav className={styles.container}>
@@ -18,7 +15,7 @@ const Nav = ({ handleMenuClick, isOpen }) => {
           className={styles.logo}
         ></img>
       </a>
-      {isMobile ? (
+      {isOpen ? (
         <span className={styles.iconWrapper}>
           <AiOutlineClose size={40} stylr onClick={handleMenuClick} />
         </span>
